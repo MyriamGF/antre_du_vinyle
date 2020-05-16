c406152ae837e16e401bbcc20e8da643a39d355f
Your Name <1837465@cstj.qc.ca>
2020-05-16 15:41:03 -0400

    Base html + ajout du carrousel

---
+++ js/carrousels.js
@@ -0,0 +1,30 @@
+// ----- CARROUSEL ACCUEIL ----- //
+$('.carrousel-accueil').slick({
+    autoplay: true,
+    arrows: true,
+    dots: true,
+    slidesToShow: 3,
+    prevArrow: '<a href="" class="slick-arrow prev-arrow">PREV</a>',
+    nextArrow: '<a href="" class="slick-arrow next-arrow">NEXT</a>',
+});
+
+// ----- CARROUSEL DÉTAILS VINYLE ----- //
+
+// $('.carrousel-vinyle').slick({
+//     autoplay: true,
+//     arrows: true,
+//     dots: true,
+//     slidesToShow: 3,
+//     prevArrow: '<a href="" class="slick-arrow prev-arrow">PREV</a>',
+//     nextArrow: '<a href="" class="slick-arrow next-arrow">NEXT</a>',
+// });
+
+// $('.carrousel-tumbnails').slick({
+//     autoplay: true,
+//     arrows: true,
+//     dots: true,
+//     slidesToShow: 3,
+//     prevArrow: '<a href="" class="slick-arrow prev-arrow">PREV</a>',
+//     nextArrow: '<a href="" class="slick-arrow next-arrow">NEXT</a>',
+// });
+
