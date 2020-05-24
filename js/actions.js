1f7f8794ca0c182b08243d5e8fb77cf66e6b6715
Your Name <1837465@cstj.qc.ca>
2020-05-24 11:55:58 -0400

    Mise en forme pop-up supprimer article

--- js/actions.js
+++ js/actions.js
@@ -21,3 +21,29 @@ function hautPage() {
 
 
 // ----- ADMIN POP-UP SUPPRIMER ----- //
+
+// Get the popUp
+var popUp = document.getElementById("mypopUp");
+
+// Get the button that opens the popUp
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the popUp
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the popUp 
+btn.onclick = function() {
+  popUp.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popUp
+span.onclick = function() {
+  popUp.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popUp, close it
+window.onclick = function(event) {
+  if (event.target == popUp) {
+    popUp.style.display = "none";
+  }
+}
